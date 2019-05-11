78399266961f1a0c05d63c751e7574959f04056a
grandalf6 <grandalf6@gmail.com>
2019-05-11 17:52:52 +0100

    remove unecessary files and opdate readme

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -4,9 +4,18 @@ import tributePage from "./images/tributePage.png"
 import randomQuoteMachine from "./images/randomQuoteMachine.png"
 import weather from "./images/weather.png"
 import pomodoro from "./images/pomodoro.png"
+import wikipedia from './images/wikipedia.png'
 
 const ProjectsData = [
-{
+	{
+		name: "Wikipedia Viewer",
+		file: wikipedia,
+		description: "Project is build with React.js, Webpack, Sass, Bootstrap and Fetch API. It allows user to search Wikipedia for any keyword and returns first 10 results including title, snippet, last date edited and word count. There is  warning for empty search, button for random article and initiation of search by Enter press.",
+		link: "https://grandalf6.github.io/wikipedia/",
+		repo: "https://github.com/grandalf6/wikipedia",
+		linkedin: true
+	},
+	{
 		name: "Weather App",
 		file: weather,
 		description: "Project is build with CSS and ReactJS. It uses 15 components, fetch API to receive data from weather service and navigator object to trace user location. When user enters website app is attempting to get user location and waits for his/her permission. Then, relevant button is displayed, and on its click data from API are requested, and user is shown these.",
--- src/SocialData.js
+++ src/SocialData.js
@@ -1,20 +1,20 @@
 const SocialData = [
-	{
-		name: "linkedin",
-		link: "https://www.linkedin.com/in/presh-rychlewicz",
-		icon: "social-media-icon fab fa-linkedin-in"
-	},
-	{
-		name: "github",
-		link: "https://github.com/grandalf6",
-		icon: "social-media-icon fab fa-github"
-	},
-	{
-		name: "resume",
-		link: "https://drive.google.com/file/d/13Tso_3AdB3CGRRNB_14YBGr_PeyxDtZ2/view?usp=sharing",
-		icon: "social-media-icon fas fa-file-alt"
-	},
-	
-]
+  {
+    name: "linkedin",
+    link: "https://www.linkedin.com/in/presh-rychlewicz",
+    icon: "social-media-icon fab fa-linkedin-in"
+  },
+  {
+    name: "github",
+    link: "https://github.com/grandalf6",
+    icon: "social-media-icon fab fa-github"
+  },
+  {
+    name: "resume",
+    link:
+      "https://drive.google.com/file/d/1_hi8Doc-VWZ-bOz-XuDThKwQJWxnU6q9/view?usp=sharing",
+    icon: "social-media-icon fas fa-file-alt"
+  }
+];
 
-export default SocialData
+export default SocialData;
