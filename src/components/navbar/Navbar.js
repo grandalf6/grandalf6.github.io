b1a38b583dc2387a6dd69716653d0265492c3d1c
unknown <grandalf6@gmail.com>
2019-05-14 17:54:46 +0100

    Navbar tidy up

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,20 +2,18 @@ import React from 'react'
 import NavbarItem from './NavbarItem'
 import SOCIAL_MEDIA_NAVBAR from '../../assets/data/SOCIAL_MEDIA_NAVBAR'
 
-const Navbar = () => {
-  return (
-    <nav
-      class="navbar is-fixed-top"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        {SOCIAL_MEDIA_NAVBAR.map((item) => (
-          <NavbarItem data={item} />
-        ))}
-      </div>
-    </nav>
-  )
-}
+const Navbar = () => (
+  <nav
+    className="navbar is-fixed-top"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div className="navbar-brand">
+      {SOCIAL_MEDIA_NAVBAR.map((item, index) => (
+        <NavbarItem data={item} key={index} />
+      ))}
+    </div>
+  </nav>
+)
 
 export default Navbar
--- src/components/navbar/NavbarItem.js
+++ src/components/navbar/NavbarItem.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import AwesomeIcon from '../generic/AwesomeIcon'
 
-const NavbarItem = ({data: {icon, link}}) => {
-  return (
-    <div class="navbar-item">
-      <a href={link} target="_blank">
-        <AwesomeIcon size={3} icon={icon} />
-      </a>
-    </div>
-  )
-}
+const NavbarItem = ({data: {icon, link}}) => (
+  <div className="navbar-item">
+    <a href={link} target="_blank">
+      <AwesomeIcon size={3} icon={icon} />
+    </a>
+  </div>
+)
 
 export default NavbarItem
