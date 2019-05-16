f1748e16bf44dd4316ab463ca985bb0a3d8df2d8
unknown <grandalf6@gmail.com>
2019-05-16 01:13:20 +0100

    reorganisation of skills data

--- src/assets/data/SKILLS_DATA.js
+++ src/assets/data/SKILLS_DATA.js
@@ -1,84 +1,76 @@
 const SKILLS_DATA = [
   {
-    skillName: "HTML5",
-    icon: "fab fa-html5",
-    color: "240, 80, 48"
+    skillName: 'HTML5',
+    icon: 'fab fa-html5',
+    type: 'skills',
+    color: 'rgb(240, 80, 48)',
   },
   {
-    skillName: "CSS3",
-    icon: "fab fa-css3",
-    color: "rgb(41, 101, 241)"
+    skillName: 'CSS3',
+    icon: 'fab fa-css3',
+    type: 'skills',
+    color: 'rgb(41, 101, 241)',
   },
   {
-    skillName: "Javascript",
-    icon: "fab fa-js",
-    color: "rgb(247, 224, 30)"
+    skillName: 'React',
+    icon: 'fab fa-react',
+    type: 'skills',
+    color: 'rgb(97, 219, 251)',
   },
   {
-    skillName: "ReactJS",
-    icon: "fab fa-react",
-    color: "rgb(97, 219, 251)"
+    skillName: 'Github',
+    icon: 'fab fa-github-square',
+    type: 'tools',
+    color: 'black',
   },
   {
-    skillName: "Bootstrap",
-    icon: "fas fa-bold",
-    color: "rgb(93, 43, 79)"
+    skillName: 'Git',
+    icon: 'fab fa-git',
+    type: 'tools',
+    color: 'rgb(240,80,48)',
   },
   {
-    skillName: "SASS",
-    icon: "fab fa-sass",
-    color: "rgb(206, 103, 154)"
+    skillName: 'Javascript',
+    icon: 'fab fa-js',
+    type: 'skills',
+    color: 'rgb(247, 224, 30)',
   },
   {
-    skillName: "Webpack",
-    icon: "far fa-square",
-    color: "rgb(117, 175, 204)"
+    skillName: 'SASS',
+    icon: 'fab fa-sass',
+    type: 'skills',
+    color: 'rgb(206, 103, 154)',
   },
   {
-    skillName: "Material Google",
-    icon: "fab fa-google",
-    color: "rgb(32, 33, 36)"
+    skillName: 'Fetch API',
+    icon: 'fas fa-database',
+    type: 'skills',
+    color: 'rgb(254, 187, 0)',
   },
   {
-    skillName: "Semantic UI",
-    icon: "fab fa-stripe-s",
-    color: "rgb(53, 189, 178)"
+    skillName: 'Firebase',
+    icon: 'fas fa-fire-alt',
+    type: 'tools',
+    color: 'rgb(255, 160,0)',
   },
   {
-    skillName: "Npm",
-    icon: "fab fa-npm",
-    color: "rgb(203, 56, 55)"
+    skillName: 'Webpack',
+    icon: 'far fa-square',
+    type: 'tools',
+    color: 'rgb(117, 175, 204)',
   },
   {
-    skillName: "API",
-    icon: "fas fa-database",
-    color: "rgb(254, 187, 0)"
+    skillName: 'D3',
+    icon: 'fas fa-grip-horizontal',
+    type: 'tools',
+    color: 'rgb(18, 184, 134)',
   },
   {
-    skillName: "Github",
-    icon: "fab fa-github-square",
-    color: "black"
+    skillName: 'Jest',
+    icon: 'fas fa-crown',
+    type: 'tools',
+    color: 'rgb(148, 64, 88)',
   },
-  {
-    skillName: "Git",
-    icon: "fab fa-git",
-    color: "rgb(240,80,48)"
-  },
-  {
-    skillName: "Wordpress",
-    icon: "fab fa-wordpress-simple",
-    color: "rgb(33, 117, 155)"
-  },
-  {
-    skillName: "D3",
-    icon: "fas fa-grip-horizontal",
-    color: "rgb(18, 184, 134)"
-  },
-  {
-    skillName: "Jest",
-    icon: "fas fa-crown",
-    color: "rgb(148, 64, 88)"
-  }
-];
+]
 
-export default SKILLS_DATA;
+export default SKILLS_DATA
