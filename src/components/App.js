aa77badc4e2808097fbacfb99820a17ddef33f6a
unknown <grandalf6@gmail.com>
2019-05-16 01:33:43 +0100

    created projects

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import About from './about/About'
 import Header from './header/Header'
 import Navbar from './navbar/Navbar'
-import About from './about/About'
+import Projects from './projects/Projects'
 
 const App = () => (
   <div>
     <Navbar />
     <Header />
     <About />
+    <Projects />
   </div>
 )
 
---
+++ src/components/projects/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SectionGeneric from '../generic/SectionGeneric'
+
+const Projects = () => (
+  <>
+    <SectionGeneric id="First" content={<p>Projects</p>} />
+  </>
+)
+
+export default Projects
