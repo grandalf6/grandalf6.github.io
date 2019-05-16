448ae05d2b53f2a6ddc4903eac1a033239a44f29
unknown <grandalf6@gmail.com>
2019-05-16 22:04:04 +0100

    fixed height of Hero

--- src/components/generic/HeroGeneric.js
+++ src/components/generic/HeroGeneric.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const HeroElement = ({content, type}) => (
-  <div className={type ? 'hero-' + type : 'hero'}>{content}</div>
+  <div className={type ? 'hero-' + type : 'hero is-fullheight-with-navbar'}>
+    {content}
+  </div>
 )
 
 export default HeroElement
