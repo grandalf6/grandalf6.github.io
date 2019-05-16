c598bea4009b2c955a82ec35676539dbc1089b8b
unknown <grandalf6@gmail.com>
2019-05-16 23:52:33 +0100

    styling of single project footer

--- src/components/projects/CardFooterItem.js
+++ src/components/projects/CardFooterItem.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const CardFooterItem = ({data: {buttonLink, buttonName}}) => (
-  <a href={buttonLink} class="card-footer-item" target="_blank">
+  <a
+    href={buttonLink}
+    class="card-footer-item has-text-weight-bold"
+    target="_blank"
+  >
     {buttonName}
   </a>
 )
