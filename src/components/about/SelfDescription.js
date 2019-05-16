b9dfdecf3ca8d6daf0cb1b3c79b8572957352c0f
unknown <grandalf6@gmail.com>
2019-05-16 01:14:53 +0100

    spelling

--- src/components/about/SelfDescription.js
+++ src/components/about/SelfDescription.js
@@ -13,7 +13,7 @@ class SelfDescription extends React.Component {
     } = this
     new TypeIt(current, {
       strings:
-        'I am front end web developer with React living in London. My adventure with web development I started about 2 years ago as a hobby and most of skills I gained through project-based learning and by just being curious. Recently I gained some certificates to structure my knowledge and confirm competence and enhance myself on the market.',
+        'I am front end web developer with React living in London. My adventure with web development started about 2 years ago as a hobby and most of skills I gained through project-based learning and by just being curious. Recently I gained some certificates to structure my knowledge and confirm competence and enhance myself on the market.',
       speed: 15,
       startDelay: 100,
       waitUntilVisible: true,
