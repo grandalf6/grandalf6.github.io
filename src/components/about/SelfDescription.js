16d039b5c8e561d4fa95f95433cac47069375664
unknown <grandalf6@gmail.com>
2019-05-17 21:12:18 +0100

    speeded up selfdescription typing

--- src/components/about/SelfDescription.js
+++ src/components/about/SelfDescription.js
@@ -14,7 +14,7 @@ class SelfDescription extends React.Component {
     new TypeIt(current, {
       strings:
         'I am front end web developer with React living in London. My adventure with web development started about 2 years ago as a hobby and most of skills I gained through project-based learning and by just being curious. Recently I gained some certificates to structure my knowledge and confirm competence and enhance myself on the market.',
-      speed: 15,
+      speed: 7,
       startDelay: 100,
       waitUntilVisible: true,
     }).go()
