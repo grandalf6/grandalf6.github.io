cb9e9fd858c9ff3cafedfbf7b238151933678388
unknown <grandalf6@gmail.com>
2019-05-24 01:22:00 +0100

    temporary footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import TextGeneric from '../generic/TextGeneric'
 
-const Footer = () => <div id="Footer">Footer</div>
+const Footer = () => (
+  <div id="Footer">
+    <TextGeneric color="white" level={true} text="grandalf6@gmail.com" />
+  </div>
+)
 
 export default Footer
