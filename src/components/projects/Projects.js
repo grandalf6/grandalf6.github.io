090df922686aef398374a1369f33344c8982a9c9
Presh Rychlewicz <grandalf6@gmail.com>
2019-05-26 21:52:56 +0100

    added svg to footer

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,18 +1,28 @@
+import mountain from '../../assets/images/svg/mountain.svg'
+import PROJECTS_DATA from '../../assets/data/PROJECTS_DATA'
 import React from 'react'
 import SectionGeneric from '../generic/SectionGeneric'
 import SingleProject from './SingleProject'
-import PROJECTS_DATA from '../../assets/data/PROJECTS_DATA'
+import tree from '../../assets/images/svg/tree.svg'
 
 const Projects = () => (
   <SectionGeneric
     name="Projects"
     colorOfBackground="hsl(0, 0%, 98%)"
     contentOfBody={
-      <div className="tile is-ancestor">
-        {PROJECTS_DATA.map((project) => (
-          <SingleProject key={project.file} data={project} />
-        ))}
-      </div>
+      <>
+        <div className="tile is-ancestor">
+          {PROJECTS_DATA.map((project) => (
+            <SingleProject key={project.file} data={project} />
+          ))}
+        </div>
+        <img
+          id="mountainFooter"
+          src={mountain}
+          alt="mountain picture yellow outline"
+        />
+        <img id="treeFooter" src={tree} alt="tree picture yellow outline" />
+      </>
     }
   />
 )
--- src/main/misc/oldjs/Footer.js
+++ src/main/misc/oldjs/Footer.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React from 'react'
+import mountain from '../assets/images/mountain.svg'
 
 const Footer = () => (
   <footer>
+    <img
+      src={mountain}
+      alt="triangle with all three sides equal"
+      height="87px"
+      width="100px"
+    />
     <h6>grandalf6@gmail.com 2019</h6>
   </footer>
-);
+)
 
-export default Footer;
+export default Footer
